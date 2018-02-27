61528a5905cd54fb2b4572699deccb25bae17168
lixinxin <895330766@qq.com>
2018-02-27 11:08:21 +0800

    Initial commit

---
+++ app/src/main/java/com/lixinxin/imageproject/MainActivity.java
@@ -0,0 +1,74 @@
+package com.lixinxin.imageproject;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
+import com.lixinxin.imageproject.utils.GlideUtils;
+
+import java.io.File;
+
+import jp.wasabeef.glide.transformations.BlurTransformation;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String IMG_URL = "http://guolin.tech/test.gif";
+
+    private ImageView mImageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.iv_header);
+
+
+//        HandlerThread mHandlerThread = new HandlerThread("handlerThread");
+//        mHandlerThread.start();
+//
+//        Handler workHadler = new Handler(mHandlerThread.getLooper()) {
+//            @Override
+//            public void handleMessage(Message msg) {
+//
+//                super.handleMessage(msg);
+//            }
+//        };
+
+
+        File cacheDir = Glide.getPhotoCacheDir(this);
+        File parentFile = cacheDir.getParentFile();
+        long size = GlideUtils.getDirSize(parentFile);
+        String sizeText = GlideUtils.byteConversionGBMBKB(size);
+        Log.e("glide_size", sizeText);
+
+
+        Glide.with(this)
+                .load(IMG_URL)
+                .apply(RequestOptions.bitmapTransform(new BlurTransformation(20)))
+                .into(mImageView);
+
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Glide.get(MainActivity.this).clearDiskCache();
+            }
+        }).start();
+    }
+
+
+    SimpleTarget<Drawable> simpleTarget = new SimpleTarget<Drawable>() {
+        @Override
+        public void onResourceReady(@NonNull Drawable resource, @Nullable Transition<? super Drawable> transition) {
+            mImageView.setImageDrawable(resource);
+        }
+    };
+}
---
+++ app/src/main/java/com/lixinxin/imageproject/utils/GlideUtils.java
@@ -0,0 +1,87 @@
+package com.lixinxin.imageproject.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.File;
+import java.text.DecimalFormat;
+
+/**
+ * Created by android on 2018/2/9.
+ */
+
+public class GlideUtils {
+
+    RequestOptions options = new RequestOptions().override(200, 100);  //设置图片大小
+
+
+    public static void load(Context context,
+                            String url,
+                            ImageView imageView,
+                            RequestOptions options) {
+        Glide.with(context)
+                .load(url)
+                .apply(options)
+                .into(imageView);
+    }
+
+    public static void load(Context context,
+                            String url,
+                            ImageView imageView) {
+        Glide.with(context)
+                .load(url)
+                .into(imageView);
+    }
+
+
+    public static String getCocheSize() {
+
+        return "";
+    }
+
+
+
+    private static final double GB = 1024L * 1024L * 1024L;// 定义GB的计算常量
+    private static final double MB = 1024L * 1024L;// 定义MB的计算常量
+    private static final double KB = 1024L;// 定义KB的计算常量
+
+    public static String byteConversionGBMBKB(double kSize) {
+        DecimalFormat df = new DecimalFormat("#.00");
+        double temp = 0;
+        if (kSize / GB >= 1) {
+            temp = kSize / GB;
+            return df.format(temp) + "GB";
+        } else if (kSize / MB >= 1) {
+            temp = kSize / MB;
+            return df.format(temp) + "MB";
+        } else if (kSize / KB >= 1) {
+            temp = kSize / KB;
+            return df.format(temp) + "KB";
+        }
+        return kSize + "B";
+    }
+
+
+    public static long getDirSize(File dir) {
+        if (dir == null) {
+            return 0;
+        }
+        if (!dir.isDirectory()) {
+            return 0;
+        }
+        long dirSize = 0;
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                dirSize += file.length();
+            } else if (file.isDirectory()) {
+                dirSize += file.length();
+                dirSize += getDirSize(file); // 递归调用继续统计
+            }
+        }
+        return dirSize;
+    }
+}
