2f0fd643df406e29ac3ce958b656ab0e2b96956f
lixinxin <895330766@qq.com>
2018-03-26 15:06:17 +0800

    升级 Android Studio 到 3.0.1

--- times-square/build/generated/source/r/androidTest/debug/com/squareup/timessquare/test/R.java
+++ times-square/build/generated/source/r/androidTest/debug/com/squareup/timessquare/test/R.java
@@ -126,45 +126,45 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int tsquare_titleTextStyle=0x7f010003;
     }
     public static final class color {
-        public static final int calendar_active_month_bg=0x7f050000;
-        public static final int calendar_bg=0x7f050001;
-        public static final int calendar_divider=0x7f050002;
-        public static final int calendar_highlighted_day_bg=0x7f050003;
-        public static final int calendar_inactive_month_bg=0x7f050004;
-        public static final int calendar_selected_day_bg=0x7f050005;
-        public static final int calendar_selected_range_bg=0x7f050006;
-        public static final int calendar_text_active=0x7f050007;
-        public static final int calendar_text_highlighted=0x7f050008;
-        public static final int calendar_text_inactive=0x7f050009;
-        public static final int calendar_text_selected=0x7f05000a;
-        public static final int calendar_text_selector=0x7f05000c;
-        public static final int calendar_text_unselectable=0x7f05000b;
+        public static final int calendar_active_month_bg=0x7f040000;
+        public static final int calendar_bg=0x7f040001;
+        public static final int calendar_divider=0x7f040002;
+        public static final int calendar_highlighted_day_bg=0x7f040003;
+        public static final int calendar_inactive_month_bg=0x7f040004;
+        public static final int calendar_selected_day_bg=0x7f040005;
+        public static final int calendar_selected_range_bg=0x7f040006;
+        public static final int calendar_text_active=0x7f040007;
+        public static final int calendar_text_highlighted=0x7f040008;
+        public static final int calendar_text_inactive=0x7f040009;
+        public static final int calendar_text_selected=0x7f04000a;
+        public static final int calendar_text_selector=0x7f04000c;
+        public static final int calendar_text_unselectable=0x7f04000b;
     }
     public static final class dimen {
-        public static final int calendar_day_headers_paddingbottom=0x7f060000;
-        public static final int calendar_month_title_bottommargin=0x7f060001;
-        public static final int calendar_month_topmargin=0x7f060002;
-        public static final int calendar_text_medium=0x7f060003;
-        public static final int calendar_text_small=0x7f060004;
+        public static final int calendar_day_headers_paddingbottom=0x7f050000;
+        public static final int calendar_month_title_bottommargin=0x7f050001;
+        public static final int calendar_month_topmargin=0x7f050002;
+        public static final int calendar_text_medium=0x7f050003;
+        public static final int calendar_text_small=0x7f050004;
     }
     public static final class drawable {
         public static final int calendar_bg_selector=0x7f020000;
     }
     public static final class id {
-        public static final int calendar_grid=0x7f070001;
-        public static final int day_names_header_row=0x7f070002;
-        public static final int day_view_adapter_class=0x7f070000;
+        public static final int calendar_grid=0x7f060001;
+        public static final int day_names_header_row=0x7f060002;
+        public static final int day_view_adapter_class=0x7f060000;
     }
     public static final class layout {
         public static final int month=0x7f030000;
         public static final int week=0x7f030001;
     }
     public static final class string {
-        public static final int day_name_format=0x7f040001;
-        public static final int invalid_date=0x7f040000;
-        public static final int month_name_format=0x7f040002;
-        public static final int month_only_name_format=0x7f040003;
-        public static final int year_only_format=0x7f040004;
+        public static final int day_name_format=0x7f070000;
+        public static final int invalid_date=0x7f070001;
+        public static final int month_name_format=0x7f070002;
+        public static final int month_only_name_format=0x7f070003;
+        public static final int year_only_format=0x7f070004;
     }
     public static final class style {
         public static final int CalendarCell=0x7f080000;
